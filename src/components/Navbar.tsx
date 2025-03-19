92e99d4ef05e3c83a804ff2dee525163cd59bcfe
Vlad Beltran <puentes.studio@gmail.com>
2025-03-19 12:03:43 +0000

    nav component styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,12 @@ type Props = {};
 
 export default function Navbar({}: Props) {
   return (
-    <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">Navbar</nav>
+    <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
+      <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
+        <p className="flex items-center justify-center gap-2">
+          <h2 className="text-gray-500 text-3xl">Weather</h2>
+        </p>
+      </div>
+    </nav>
   );
 }
