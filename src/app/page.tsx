6ed5e3b9cb373f36df752a68627e15a1e0321dc4
Vlad Beltran <puentes.studio@gmail.com>
2025-03-19 16:48:19 +0000

    api type, and react query tanstack installed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,61 @@
 import Navbar from "@/components/Navbar";
 
+// https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${process.env.PUBLIC_WEATHER_KEY}&cnt=56
+// https://api.openweathermap.org/data/2.5/forecast?q=london&appid=1ea77c597e499c75c3a836e711a8b663&cnt=56
+
+interface WeatherData {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: WeatherDetail[];
+  city: {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    population: number;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  };
+}
+
+interface WeatherDetail {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  sys: { pod: string };
+  dt_txt: string;
+}
+
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
