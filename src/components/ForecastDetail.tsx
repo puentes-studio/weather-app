ff97e1293499c7f5bf293326ac35be508c6df70e
Vlad Beltran <puentes.studio@gmail.com>
2025-03-25 19:30:32 +0000

    forecast left part done

--- src/components/ForecastDetail.tsx
+++ src/components/ForecastDetail.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "./Container";
 import WeatherIcon from "./WeatherIcon";
 import { WeatherDetailsProps } from "./WeatherDetails";
+import { ToCelsius } from "@/utils/convertToCelcius";
 
 export interface ForecastWeatherDetailsProps extends WeatherDetailsProps {
   weatherIcon: string;
@@ -14,12 +15,23 @@ export interface ForecastWeatherDetailsProps extends WeatherDetailsProps {
   description: string;
 }
 
-export default function ForecastDetail({}: Props) {
+export default function ForecastDetail(props: ForecastWeatherDetailsProps) {
   return (
     <Container className=" gap-4">
       <section className="flex gap-4 items-center px-4">
         <div>
-          <WeatherIcon />
+          <WeatherIcon iconName={WeatherIcon} />
+          <p>{date}</p>
+          <p className="text-sm">{day}</p>
+        </div>
+
+        <div className="flex flex-col px-4">
+          <span className="text-5xl">{ToCelsius(temp ?? 0)}º</span>
+          <p className="text-xs space-x-1 whitespace-nowrap">
+            <span>Feels like</span>
+            <span>{ToCelsius(temp ?? 0)}º</span>
+          </p>
+          <p className="capitalize">{desciption}</p>
         </div>
       </section>
     </Container>
