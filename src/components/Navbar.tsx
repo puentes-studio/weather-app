34c83ac2bf3a554a0dd3ac69f8fb7ab5af55557c
Vlad Beltran <puentes.studio@gmail.com>
2025-04-02 18:25:29 +0100

    search box function for suggestions and submit done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,66 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { TiWeatherPartlySunny } from "react-icons/ti";
 import { CiLocationOn } from "react-icons/ci";
 import { RiUserLocationLine } from "react-icons/ri";
 import Searchbox from "./Searchbox";
+import axios from "axios";
 
 type Props = {};
 
+const API_KEY = process.env.NEXT_PUBLIC_WEATHER_KEY;
+
 export default function Navbar({}: Props) {
+  const [city, setCity] = useState("");
+  const [error, setError] = useState("");
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  async function handleInputChange(value: string) {
+    setCity(value);
+
+    if (value.length >= 3) {
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=5&appid=${API_KEY}`
+        );
+
+        const suggestions = response.data.map(
+          (item: any) => `${item.name}, ${item.country}` // Shows "City, Country"
+        );
+
+        setSuggestions(suggestions);
+        setShowSuggestions(suggestions.length > 0);
+        setError(suggestions.length === 0 ? "No matching locations found" : "");
+      } catch (error) {
+        setSuggestions([]);
+        setShowSuggestions(false);
+        setError("Failed to fetch location data");
+      }
+    } else {
+      setSuggestions([]);
+      setShowSuggestions(false);
+      setError("");
+    }
+  }
+
+  function handleSuggestionsClick(item: string) {
+    setCity(item);
+    setShowSuggestions(false);
+  }
+
+  function handleSubmitSearch(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (suggestions.length === 0) {
+      setError("Location not found");
+      return;
+    } else {
+      setError("");
+      setShowSuggestions(false);
+    }
+  }
+
   return (
     <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
       <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
@@ -18,12 +72,56 @@ export default function Navbar({}: Props) {
           <CiLocationOn className="text-2xl text-black hover:opacity-75 cursor-pointer" />
           <RiUserLocationLine className="text-3xl text-black hover:opacity-75 cursor-pointer" />
           <p className="text-slate-900/80 text-sm"> Sidcup </p>
-          <div>
-            <Searchbox />
-            {/* Searchbox  */}
+          <div className="relative">
+            <Searchbox
+              value={city}
+              onSubmit={handleSubmitSearch}
+              onChange={(e) => handleInputChange(e.target.value)}
+            />
+            <SuggestionBox
+              {...{
+                suggestions,
+                showSuggestions,
+                handleSuggestionsClick,
+                error,
+              }}
+            />
           </div>
         </section>
       </div>
     </nav>
+  );
+}
+
+function SuggestionBox({
+  suggestions,
+  showSuggestions,
+  handleSuggestionsClick,
+  error,
+}: {
+  suggestions: string[];
+  showSuggestions: boolean;
+  handleSuggestionsClick: (item: string) => void;
+  error: string;
+}) {
+  return (
+    <>
+      {((showSuggestions && suggestions.length > 1) || error) && (
+        <ul className="mb-4 bg-white absolute border top-[44px] left-0 border-gray-300 rounded-md min-w-[200px] flex flex-col gap-1 py-2 px-2">
+          {error && suggestions.length > 1 && (
+            <li className="p-1 text-red-700">{error}</li>
+          )}
+          {suggestions.map((item, i) => (
+            <li
+              key={i}
+              onClick={() => handleSuggestionsClick(item)}
+              className="cursor-pointer p-1 rounded hover:bg-gray-200"
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
